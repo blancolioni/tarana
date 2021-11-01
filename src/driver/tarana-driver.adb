f395dd235b609c4d3ff454ff3cf936d5bbb0109e
Fraser Wilson <fraser@thewhitelion.eu>
2021-11-01 12:30:18 +0100

    Create tarana-driver.adb

---
+++ src/driver/tarana-driver.adb
@@ -0,0 +1,10 @@
+with Tarana.Signal_Store;
+with Tarana.Paths;
+
+pragma Unreferenced (Tarana.Signal_Store);
+pragma Unreferenced (Tarana.Paths);
+
+procedure Tarana.Driver is
+begin
+   null;
+end Tarana.Driver;
